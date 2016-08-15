0a97079099d6665c850d3d5c1847667e3468f4ec
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-15 16:24:02 +0300

    created booking review offer model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815121353) do
+ActiveRecord::Schema.define(version: 20160815130943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "full_price"
+    t.integer  "users_id"
+    t.integer  "offers_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offers_id"], name: "index_bookings_on_offers_id", using: :btree
+    t.index ["users_id"], name: "index_bookings_on_users_id", using: :btree
+  end
+
+  create_table "offers", force: :cascade do |t|
+    t.string   "location"
+    t.integer  "price_per_day"
+    t.string   "superpower"
+    t.string   "video"
+    t.integer  "users_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["users_id"], name: "index_offers_on_users_id", using: :btree
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "rating"
+    t.integer  "bookings_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["bookings_id"], name: "index_reviews_on_bookings_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,4 +68,8 @@ ActiveRecord::Schema.define(version: 20160815121353) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "offers", column: "offers_id"
+  add_foreign_key "bookings", "users", column: "users_id"
+  add_foreign_key "offers", "users", column: "users_id"
+  add_foreign_key "reviews", "bookings", column: "bookings_id"
 end
