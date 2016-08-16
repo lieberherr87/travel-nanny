3ad3954f959e68604f5bb7fb18c397b3263c8f39
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-16 16:15:23 +0300

    created pages controller, homepage view

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,8 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
 
-  has_many :reviews
-
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :full_price, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
+  get 'pages/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :bookings
 
   resources :offers
   resources :bookings do
-    resources :reviews, only: [:index, :show, :new, :create]
+    resources :reviews, only: [:show, :new, :create]
   end
 
   # resources :bookings do
