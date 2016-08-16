bea9eabef763f96eaec75af9b90522eb84588081
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-16 11:50:44 +0300

    created routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :bookings
 
-  root to: "offers#index"
+  resources :users do
+    resources :offers
+    resources :bookings do
+      resources :reviews, only: [:index, :show, :new, :create]
+    end
+  end
+
+  # resources :bookings do
+  #   resources :reviews, only: [:index, :show, :new, :create]
+  # end
+
+  root to: "pages#index"
 
 end
