52004706f76cb9547b79969dec7d5b655a8601f7
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2016-08-16 15:41:36 +0300

    changed model and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+User.destroy_all
 
-user1 = User.create(first_name:"First name 1", last_name: "Last name 1", email: "test@mail.com", password: "TESTTEST", encrypted_password:"TESTESTETET")
-user2 = User.create(first_name:"First name 2", last_name: "Last name 2", email: "test@mail.com", password: "TESTTEST", encrypted_password:"TESTESTETET")
-user3 = User.create(first_name:"First name 3", last_name: "Last name 3", email: "test@mail.com", password: "TESTTEST", encrypted_password:"TESTESTETET")
 
-offer1 = Offer.create(location:"Züri", price_per_day: 100, user_id:3)
-offer2 = Offer.create(location:"London", price_per_day: 200, user_id:1)
-offer3 = Offer.create(location:"Paris", price_per_day: 50, user_id:2)
+user1 = User.create(first_name:"First name 1", last_name: "Last name 1", email: "test5@mail.com", password: "TESTTEST")
+user2 = User.create(first_name:"First name 2", last_name: "Last name 2", email: "test1@mail.com", password: "TESTTEST")
+user3 = User.create(first_name:"First name 3", last_name: "Last name 3", email: "test2@mail.com", password: "TESTTEST")
 
-booking1 = Booking.create(start_date: 20091201, end_date: 20091202, full_price:"300", user_id: 3)
-booking2 = Booking.create(start_date: 20101201, end_date: 20101202, full_price:"400", user_id: 1)
-booking3 = Booking.create(start_date: 20111201, end_date: 20111202, full_price:"500", user_id: 2)
+offer1 = user1.create_offer(location:"Züri", price_per_day: 100)
+offer2 = user2.create_offer(location:"London", price_per_day: 200)
+offer3 = user3.create_offer(location:"Paris", price_per_day: 50)
 
-review1 = Review.create(content: "blabliblureview", rating: 3, booking_id: 2)
-review2 = Review.create(content: "blabliblureview", rating: 1, booking_id: 1)
-review3 = Review.create(content: "blabliblureview", rating: 5, booking_id: 3)
+booking1 = user1.bookings.create(start_date: 20091201, end_date: 20091202, full_price:"300", offer: offer1)
+booking2 = user2.bookings.create(start_date: 20101201, end_date: 20101202, full_price:"400", offer: offer2)
+booking3 = user3.bookings.create(start_date: 20111201, end_date: 20111202, full_price:"500", offer: offer3)
+
+review1 = booking1.reviews.create(content: "blabliblureview", rating: 3)
+review2 = booking2.reviews.create(content: "blabliblureview", rating: 1)
+review3 = booking3.reviews.create(content: "blabliblureview", rating: 5)
