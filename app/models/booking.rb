9fdc507fbb83b83b1d84230eae7012304f541689
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2016-08-16 13:46:23 +0300

    finished booking model with validations and relations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :offer
+
+  has_many :reviews
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :full_price, presence: true
 end
+
