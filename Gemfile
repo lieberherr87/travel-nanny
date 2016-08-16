e3b32f5b05d0a474a5c179cc7874fa9e7608f2df
Vanessa Oppowa <vanessa.oppowa87@gmail.com>
2016-08-16 13:24:57 +0300

    Add figaro - Protect my secret data in config/application.yml

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+#
+gem 'figaro'
 
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+
+  has_many :bookings, dependent: :destroy
+  has_one :offers, dependent: :destroy
+
+  mount_uploader :photo, PhotoUploader
+
 end
