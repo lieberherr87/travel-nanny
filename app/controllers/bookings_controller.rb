177e86505cf4a8415c260bd67409da32bd6fe431
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-16 13:27:28 +0300

    changed routes removed user path. created booking controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,38 @@
+class BookingsController < ApplicationController
+  before_action set_booking
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_booking
+      @booking = current_user.booking.find_by(id: params[:id])
+
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def booking_params
+      params.require(:booking).permit(:start_date, :end_date, :full_price, :users_id, :offers_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :bookings
 
-  resources :users do
-    resources :offers
-    resources :bookings do
-      resources :reviews, only: [:index, :show, :new, :create]
-    end
+  resources :offers
+  resources :bookings do
+    resources :reviews, only: [:index, :show, :new, :create]
   end
 
   # resources :bookings do
