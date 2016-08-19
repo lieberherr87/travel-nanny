7deb899f68e3da5d72d03bf48dcddd601bf48780
Stefan Lieberherr <lieberherr87@gmail.com>
2016-08-19 13:04:27 +0300

    added review link to booking index booking show

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
+
 Rails.application.routes.draw do
 
 
 
   get '/search' => 'offers#search'
 
+  get 'review/show'
+
+  get 'review/new'
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :bookings
+
   resources :offers do
-    resources :bookings, only: [:index, :new, :create, :destroy]
+    resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:index, :show, :destroy]
 
   resources :bookings do
-    resources :reviews, only: [:show, :new, :create]
+    resources :reviews, only: [:index, :show, :new, :create]
   end
 
   root "pages#index"
 
-end
